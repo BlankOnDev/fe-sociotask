465c3898524ccbd6f7bbd27d073eaf1382dd1205
harundarat <harundarat@gmail.com>
2025-11-30 20:23:46 +0700

    feat: implement api on register flow

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -8,93 +8,95 @@ import { AxiosError } from "axios";
 import { useSetAtom } from "jotai";
 
 type LoginRequestParams = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 };
 
 type LoginResponse = {
-    data: {
-        token: string;
-    };
-    message: string;
-    status: string;
+  data: {
+    token: string;
+  };
+  message: string;
+  status: string;
 };
 
-export const login = async (params: LoginRequestParams): Promise<LoginResponse> => {
-    try {
-        const response = await axiosInstance.post("/login", params);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+export const login = async (
+  params: LoginRequestParams
+): Promise<LoginResponse> => {
+  try {
+    const response = await axiosInstance.post("/login", params);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 type UseLoginParams = {
-    mutationConfig?: MutationConfig<typeof login>;
+  mutationConfig?: MutationConfig<typeof login>;
 };
 
 export const useLogin = (params: UseLoginParams = {}) => {
-    const setTokenStorage = useSetAtom(tokenStorageAtom);
-    const setAuthedUser = useSetAtom(authedUserAtom)
-    const toasterId = "login-action";
-    return useMutation({
-        ...params.mutationConfig,
-        mutationFn: login,
-        onMutate: () => {
-            toaster.loading({
-                id: toasterId,
-                title: "Logging in...",
-                description: "Please wait while we log you in.",
-            });
-        },
-        onSuccess: (data, variables, onMutateResult, context) => {
-            setTokenStorage(data.data.token);
-            getAuthedUser({ token: data.data.token })
-                .then((res) => {
-                    setAuthedUser(res)
-                    toaster.update(toasterId, {
-                        type: "success",
-                        title: "Login Successful",
-                        description: "You have been logged in successfully.",
-                    });
-                    params.mutationConfig?.onSuccess?.(
-                        data,
-                        variables,
-                        onMutateResult,
-                        context
-                    );
-                })
-                .catch((err) => {
-                    console.log({err});
-                    toaster.update(toasterId, {
-                        type: "error",
-                        title: "Login Failed",
-                        description: "Sorry we can't now process your loggin, please try again"
-                    })
-                })
-        },
-        onError(error, variables, onMutateResult, context) {
-            const axiosError = error as AxiosError;
-            let message;
-            if (axiosError.status == 401)
-                message = "Invalid email or password.";
-            else if (axiosError.status == 404)
-                message = "Your account (email) was not found.";
-            toaster.update(toasterId, {
-                type: "error",
-                title: "Login Failed",
-                description:
-                    message ||
-                    (axiosError instanceof Error
-                        ? axiosError.message
-                        : "An unexpected error occurred. Please try again."),
-            });
-            params.mutationConfig?.onError?.(
-                error,
-                variables,
-                onMutateResult,
-                context
-            );
-        },
-    });
+  const setTokenStorage = useSetAtom(tokenStorageAtom);
+  const setAuthedUser = useSetAtom(authedUserAtom);
+  const toasterId = "login-action";
+  return useMutation({
+    ...params.mutationConfig,
+    mutationFn: login,
+    onMutate: () => {
+      toaster.loading({
+        id: toasterId,
+        title: "Logging in...",
+        description: "Please wait while we log you in.",
+      });
+    },
+    onSuccess: (data, variables, onMutateResult, context) => {
+      setTokenStorage(data.data.token);
+      getAuthedUser({ token: data.data.token })
+        .then((res) => {
+          setAuthedUser(res);
+          toaster.update(toasterId, {
+            type: "success",
+            title: "Login Successful",
+            description: "You have been logged in successfully.",
+          });
+          params.mutationConfig?.onSuccess?.(
+            data,
+            variables,
+            onMutateResult,
+            context
+          );
+        })
+        .catch((err) => {
+          console.log({ err });
+          toaster.update(toasterId, {
+            type: "error",
+            title: "Login Failed",
+            description:
+              "Sorry we can't now process your loggin, please try again",
+          });
+        });
+    },
+    onError(error, variables, onMutateResult, context) {
+      const axiosError = error as AxiosError;
+      let message;
+      if (axiosError.status == 401) message = "Invalid email or password.";
+      else if (axiosError.status == 404)
+        message = "Your account (email) was not found.";
+      toaster.update(toasterId, {
+        type: "error",
+        title: "Login Failed",
+        description:
+          message ||
+          (axiosError instanceof Error
+            ? axiosError.message
+            : "An unexpected error occurred. Please try again."),
+      });
+      params.mutationConfig?.onError?.(
+        error,
+        variables,
+        onMutateResult,
+        context
+      );
+    },
+  });
 };
